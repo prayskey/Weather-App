b9b43286a55042c45e957311dabae9a7257521ca
Prayskey <prayskeyo@gmail.com>
2025-11-06 18:36:18 +0100

    Add app.js file to index.html

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const apiKey = "78ccb0d83d874c5da35191502251308"; //My API key
+const apiKey = "33aa39b727fa4ea9b8b172149250611"; //My API key
 
+async function fetchWeatherData() {
+    const res = await fetch(apiKey);
+    console.log(res);
+
+    //const data = res.data();
+
+
+    
+/*
+ if (!data.ok) {
+        throw new Error("404 Error \n file not found");
+    }
+*/
+
+   
+}
+
+fetchWeatherData();
