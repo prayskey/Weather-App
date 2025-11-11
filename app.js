0015e52f11a469c897f51ec97cbb97372cdfe64b
Prayskey <prayskeyo@gmail.com>
2025-11-11 23:25:38 +0100

    Add time and greetings features to weather app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+
+
 const search_bar = document.querySelector(".search-bar");
 const search_btn = document.querySelector(".search-btn");
 const location_btn = document.querySelector(".location-btn");
 const temperature_text = document.querySelector(".big-temperature-text");
 const user_city_text = document.querySelector(".user_city");
 const user_country_text = document.querySelector(".user_country");
+const location_current_time = document.querySelectorAll(".location_current_time");
 const feels_like_text = document.querySelectorAll(".feels-like-text");
 const humidity_text = document.querySelector(".humidity-text");
 const wind_speed_text = document.querySelector(".wind-speed-text");
@@ -14,7 +17,7 @@ const sunset_text = document.querySelector(".sunset-text");
 const precipitation_text = document.querySelector(".precipitation-text");
 const weather_condition_text = document.querySelector(".weather-condition-text"); // e.g., "Sunny", "Cloudy"
 const setting_icon = document.querySelector(".lucide-settings-icon");
-const greetings_text = document.querySelector(".greetings-text");
+const greetings_text = document.querySelectorAll(".greetings-text");
 const temp_unit_elements = document.querySelectorAll(".temp_unit");
 const sunrise_amOrPm_text = document.querySelector(".sunrise_amOrPm_text");
 const sunset_amOrPm_text = document.querySelector(".sunset_amOrPm_text");
@@ -22,10 +25,8 @@ const sunset_amOrPm_text = document.querySelector(".sunset_amOrPm_text");
 const apiKey = "33aa39b727fa4ea9b8b172149250611"; //My API key
 let country = null; // current location of the user
 
-window.addEventListener("load", getcurrentLocation);
 
-//fetchWeatherData();
-
+window.addEventListener("load", getcurrentLocationProperties);
 
 // Fetch weather data from the API
 async function fetchWeatherData(lat, lon) {
@@ -38,9 +39,9 @@ async function fetchWeatherData(lat, lon) {
         } else console.log("Forecast data fetched successfully.");
 
         const data = await response.json();
+        const currentHour = parseInt(data.location.localtime.split(" ")[1].split(":")[0]);
 
         // Update the UI with fetched data
-
         temperature_text.innerHTML = `${data.current.temp_c}<span class="temp_unit">°C</span>`;
         feels_like_text.textContent = Math.round(data.current.feelslike_c);
         humidity_text.textContent = data.current.humidity;
@@ -51,14 +52,35 @@ async function fetchWeatherData(lat, lon) {
         weather_condition_text.textContent = data.current.condition.text;
         user_city_text.textContent = data.location.name;
         user_country_text.textContent = data.location.country;
+        location_current_time.forEach(element => element.textContent = getLocationTime());
         feels_like_text.forEach(element => element.textContent = data.current.feelslike_c);
         temp_unit_elements.forEach(element => { element.textContent = "°C" });
-        greetings_text.textContent = "";
+        greetings_text.forEach(element => element.textContent = getGreetings(currentHour));
         sunrise_text.textContent = "";
         sunset_text.textContent = "";
         sunrise_amOrPm_text.textContent = "";
         sunset_amOrPm_text.textContent = "";
 
+
+        // Get local time of the Api fetch location
+        function getLocationTime() {
+            function zeroPadding(unit) {
+                unit = unit.toString();
+                return unit.length < 2 ? "0" + unit : unit;
+            }
+
+            const localTime = data.location.localtime_epoch;
+
+            const date = new Date(localTime * 1000).toISOString();
+            const timePart = date.split("T")[1].split(".")[0];
+            const timeParts = timePart.split(":");
+            const hours = zeroPadding(parseInt(timeParts[0]));
+            const minutes = zeroPadding(parseInt(timeParts[1]));
+            const seconds = zeroPadding(parseInt(timeParts[2]));
+
+
+            return `${hours}:${minutes}:${seconds}`;
+        }
 
     } catch {
         console.error("There was a problem fetching the weather data.");
@@ -66,23 +88,35 @@ async function fetchWeatherData(lat, lon) {
 }
 
 // Get the current location of the user
-function getcurrentLocation() {
-
-    navigator.geolocation.getCurrentPosition(successcallBack, errorCallback, {
-        enableHighAccuracy: true,
-        maximumAge: 0
-    });
+function getcurrentLocationProperties() {
 
     function successcallBack(position) {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
 
         fetchWeatherData(lat, lon);
-
-        console.log(`Latitude: ${lat}, Longitude: ${lon}`);
     }
+
     function errorCallback(error) {
         console.error(`ERROR ${error.code}: ${error.message}`);
     }
 
+    navigator.geolocation.getCurrentPosition(successcallBack, errorCallback, {
+        enableHighAccuracy: true,
+        maximumAge: 0
+    });
+
+
+
+}
+
+// Get greetings based on the current hour
+function getGreetings(currentHour) {
+    if (currentHour < 12) {
+        return "Good Morning";
+    } else if (currentHour >= 12 && currentHour < 16) {
+        return "Good Afternoon";
+    } else {
+        return "Good Evening";
+    }
 }
